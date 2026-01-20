3151b36056f4aaab0726404323b43c415c4bee0b
Johnny <jkfaris@gmail.com>
2026-01-20 15:09:27 -0800

    remove Footer component content, returning null instead of rendering footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,4 @@
 export default function Footer({ timeout }) {
-  return (
-    <footer id="footer" style={timeout ? {display: 'none'} : {}}>
-      <p className="copyright">Refactored to Next.js by Johnny Faris | Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
-    </footer>
-  )
+  return null
 }
 
